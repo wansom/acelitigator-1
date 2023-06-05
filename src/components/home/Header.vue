41fdd941fca5658c5f5242f1ddb1aba6e47f5088
wansom <ochiengwarren10@gmail.com>
2023-06-05 09:48:39 +0300

    update court design

--- src/components/home/Header.vue
+++ src/components/home/Header.vue
@@ -97,7 +97,7 @@
               <li class="active">
                 <router-link to="/find-a-lawyer">FIND LAWYERS</router-link>
               </li>
-              <li><a href="//kenyalaw.org" target="blank">Know Your Courts</a></li>
+              <li><a href="/know-your-courts">Know Your Courts</a></li>
               <li>
                 <a href="#">Resources</a>
                 <ul class="dropdown">
--- src/views/Know-Your-Courts.vue
+++ src/views/Know-Your-Courts.vue
@@ -1,13 +1,49 @@
 <template>
   <div>
     <Header></Header>
-    <Breadcrum></Breadcrum>
-    <section class="blog-section spad">
-      <div class="container">
-        <div class="row">
-          <KnowYourCourtsSidebar />
-          <KnowYourCourtsContent />
-        </div>
+    <section class="">
+      <div class="container mx-auto py-10 px-6">
+    <a-tabs type="card">
+      <a-tab-pane key="1" tab="Federal Courts"  class="py-10">
+        <a-tabs default-active-key="1" :tab-position="tabPosition">
+      <a-tab-pane key="1" tab="Tab 1">
+        Content of Tab 1
+      </a-tab-pane>
+      <a-tab-pane key="2" tab="Tab 2">
+        Content of Tab 2
+      </a-tab-pane>
+      <a-tab-pane key="3" tab="Tab 3">
+        Content of Tab 3
+      </a-tab-pane>
+    </a-tabs>
+      </a-tab-pane>
+      <a-tab-pane key="2" tab="State Courts "  class="py-10">
+        <a-tabs default-active-key="1" :tab-position="tabPosition">
+      <a-tab-pane key="1" tab="Tab 1">
+        Content of Tab 1
+      </a-tab-pane>
+      <a-tab-pane key="2" tab="Tab 2">
+        Content of Tab 2
+      </a-tab-pane>
+      <a-tab-pane key="3" tab="Tab 3">
+        Content of Tab 3
+      </a-tab-pane>
+    </a-tabs>
+      </a-tab-pane>
+      <a-tab-pane key="3" tab="Tribunals" class="py-10">
+        <a-tabs default-active-key="1" :tab-position="tabPosition">
+      <a-tab-pane key="1" tab="Tab 1">
+        Content of Tab 1
+      </a-tab-pane>
+      <a-tab-pane key="2" tab="Tab 2">
+        Content of Tab 2
+      </a-tab-pane>
+      <a-tab-pane key="3" tab="Tab 3">
+        Content of Tab 3
+      </a-tab-pane>
+    </a-tabs>
+      </a-tab-pane>
+    </a-tabs>
       </div>
     </section>
     <Footer></Footer>
@@ -21,6 +57,11 @@ import Breadcrum from "../components/Widgets/Breadcrum.vue";
 import KnowYourCourtsSidebar from "../components/Sidebars/Know-Your-Courts-Sidebar.vue";
 import KnowYourCourtsContent from "../components/Know-Your-Courts/Know-Your-Courts-Content.vue";
 export default {
+  data() {
+    return {
+      tabPosition: 'left',
+    };
+  },
   components: {
     Header,
     Footer,
