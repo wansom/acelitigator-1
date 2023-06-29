29bfbe250ec99f1d49b7832dfcfc0af4c779ae08
wansom <ochiengwarren10@gmail.com>
2023-06-29 11:36:51 +0300

    change map

--- src/main.js
+++ src/main.js
@@ -28,7 +28,7 @@ import VueMeta from 'vue-meta'
 
 Vue.use(VueMeta)
 
-
+Vue.use(VueMeta)
 Vue.use(Antd);
 Vue.use(VueSocialSharing);
 Vue.use(VueCarousel);
@@ -47,6 +47,20 @@ auth.onAuthStateChanged((user) => {
     app = new Vue({
       router,
       store,
+      metaInfo: {
+        // if no title is specified, "Dial a Lawyer" will be used as a default title
+        title: 'Dial a Lawyer', 
+        // all titles will be injected into this template
+        titleTemplate: '%s | Dial a Lawyer',
+        meta: [
+          { property: 'og:site_name', content: 'Dial a Lawyer' },
+          { property: 'og:image', content: 'https://dialalawyer.africa/img/banner.251176c6.jpg'},
+          { property: 'og:description', content: 'Our mission is to aid access to justice across Africa by linking verified legal professionals in the justice sector to the market' },
+          { name: 'twitter:site', content: '@dialalawyer' },
+          { name: 'twitter:description', content: 'Our mission is to aid access to justice across Africa by linking verified legal professionals in the justice sector to the market' },
+          // other meta tags...
+        ]
+      },
       render: (h) => h(App),
     }).$mount("#app");
   }
