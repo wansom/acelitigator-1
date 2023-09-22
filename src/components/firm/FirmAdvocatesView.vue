81abbe6d9ef1febfe8f315af48e68d8aa34ae5d2
wansom <ochiengwarren10@gmail.com>
2023-09-22 12:28:27 +0300

    add avatar

--- src/components/firm/FirmAdvocatesView.vue
+++ src/components/firm/FirmAdvocatesView.vue
@@ -12,18 +12,23 @@
 				</a-col>
 			</a-row>
 		</template>
-    <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="grid grid-cols-1 md:grid-cols-4 gap-4" >
+      <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" v-for="(member,index) of user.teamMembers" :key="index">
 
-    <div class="flex flex-col items-center pb-10">
-        <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="/images/stella.jpg" alt="Bonnie image"/>
-        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
-        <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
-        <!-- <div class="flex mt-4 space-x-3 md:mt-6">
-            <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add friend</a>
-            <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">Message</a>
-        </div> -->
+<div class="flex flex-col items-center pb-10">
+    <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="/images/profiles/avatar.png" alt="Bonnie image"/>
+    <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{{ member.name }}</h5>
+    <span class="text-sm text-gray-500 dark:text-gray-400">{{ member.job_title }}</span>
+    <!-- <div class="flex mt-4 space-x-3 md:mt-6">
+        <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add friend</a>
+        <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">Message</a>
+    </div> -->
+</div>
+
+</div>
     </div>
-    <a-drawer
+
+<a-drawer
       title="Add Team Member"
       :width="720"
       :visible="visible"
@@ -116,12 +121,11 @@
         <a-button :style="{ marginRight: '8px' }" @click="onClose">
           Cancel
         </a-button>
-        <a-button type="primary" @click="onClose">
+        <a-button type="primary" @click="handleSubmit">
           Submit
         </a-button>
       </div>
     </a-drawer>
-</div>
 	</a-card>
 	<!-- / Authors Table Card -->
 
@@ -133,7 +137,7 @@ import { storage } from "../../database";
 import { Cropper, Preview } from "vue-advanced-cropper";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import moment from "moment";
-import { updateAdvocate } from "../../database/firestore";
+import { updateAdvocate, updateTeamMember } from "../../database/firestore";
 function getMimeType(file, fallback = null) {
 	const byteArray = (new Uint8Array(file)).subarray(0, 4);
     let header = '';
@@ -315,8 +319,7 @@ function getMimeType(file, fallback = null) {
             job_title: values.job_title ?? "",
             biography:values.biography??""
           };
-
-          this.$store.dispatch("updateUser", payload);
+          updateTeamMember(this.user.id,payload,'add')
         }
       });
     },
